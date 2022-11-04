b063ffc55696789ae17ee6d778c84b6228a9165d
Jason <j450nx@gmail.com>
2022-11-04 16:52:56 -0400

    first commit

---
+++ src/test/java/Test01_GET.java
@@ -0,0 +1,34 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+// static import
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+import io.restassured.response.Response;
+
+public class Test01_GET {
+	
+	String request = "https://reqres.in/api/users?page=2";
+	
+	@Test
+	void test_01() {
+		Response response = get(request);
+		System.out.println(response.asString());
+		System.out.println(response.getBody().asString());
+		System.out.println(response.getStatusCode());
+		System.out.println(response.getHeader("content-type"));
+		System.out.println(response.getTime());
+		
+		int statusCode = response.getStatusCode();
+		Assert.assertEquals(statusCode, 200);
+	}
+	
+	@Test	
+	void test_02() {
+		given()
+			.get(request)
+		.then()
+			.statusCode(200)
+			.body("data.id[0]", equalTo(7));
+	}
+}
